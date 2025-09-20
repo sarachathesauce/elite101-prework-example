4be80fd1ace5b93f9d652232a2c46adea6cbd824
Sarah Naaz <106031755+sarachathesauce@users.noreply.github.com>
2025-09-20 02:28:06 +0000

    added prework reqs

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
-print("Welcome to the I:SO chatbot!")
+print("Welcome to the FTC I:SO chatbot!")
 name = input("What is your name?")
 age = input( name + ", How old are you?")
+print("Oh, to be "+ age +"again!")
+print("How can I help you today," + name + "?")
+
+#options:
+while True:
+  print("1. See a team's stats")
+  print("2. Check the latest match in a league")
+  print("3. Check a match's score")
+  print("4. Exit")
+  
+  choice = input("Please select an option from 1-4:")
+
+  if choice == "1":
+    print("Option 1: Still in development!")
+  elif choice == "2":
+    print("Option 2: Still in development!")
+  elif choice == "3":
+    print("Option 3: Still in development!")
+  elif choice == "4":
+    print("Alright then, Goodbye," + name + "!")
+    break
